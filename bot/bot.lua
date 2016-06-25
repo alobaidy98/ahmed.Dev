59632afb141ce56180b09812b0064476105be40c
Ahmed_Alobaidy <alobaidy607@gmail.com>
2016-06-24 18:53:35 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -212,27 +212,27 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-	"admin",
-    "onservice",
-    "inrealm",
-    "ingroup",
-    "inpm",
-    "banhammer",
-    "stats",
-    "anti_spam",
-    "owners",
-    "arabic_lock",
-    "set",
-    "get",
-    "broadcast",
-    "invite",
+	  "addsudo",
+    "admin",
     "all",
-    "leave_ban",
-	"supergroup",
-	"whitelist",
-	"msg_checks"
+    "banhammer",
+    "bbrplay",
+    "block",
+    "broadcast",
+    "commands",
+    "Feedback",
+    "supergroup",
+    "super-info",
+    "ingroup",
+    "lock-fwd",
+    "msg_checks",
+    "me",
+    "id2",
+  	"replay",
+	  "sticker",
+  	"invite"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {10012023,94465349,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
